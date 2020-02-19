6747a28dea8f3e2447c996ba83c4671ee17066f5
Malte Kiefer <maltekiefer1987@gmail.com>
2020-02-19 09:50:25 +0100

    new update
    Signed-off-by: Malte Kiefer <maltekiefer1987@gmail.com>

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
 
   $(".card-header-icon").click(function() {
     $(this).parent().next().toggle();
-    $(this).find("i").toggleClass("fa-angle-up fa-angle-down"); 
+    $(this).find("i").toggleClass("fa-angle-up fa-angle-down");
+    $('html, body').animate({
+        scrollTop: $(this).offset().top
+    }, 500);
   });
 });
